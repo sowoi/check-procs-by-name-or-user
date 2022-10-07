36a075c625b326e3228d5d4cb95c9c3e8917c237
sowoi <10256263+sowoi@users.noreply.github.com>
2022-10-07 15:03:51 +0200

    typos

--- check_procs_by_name_or_user.ps1
+++ check_procs_by_name_or_user.ps1
@@ -18,8 +18,8 @@ $returnStateUnknown = 3
 
 $countProcs = Get-Process -Name $param1 -IncludeUserName -ErrorAction SilentlyContinue| Measure-Object | Select -Expand count
 
+Queries with "desired user" input
 if($param3){
-$overallProcs = Get-Process -Name $param1 -ErrorAction SilentlyContinue| Measure-Object | Select -Expand count
 $userProcs = Get-Process -Name $param1 -IncludeUserName -ErrorAction SilentlyContinue| Where-Object UserName -Like $param3* |  Measure-Object | Select -Expand count
 $userProcsFalse = Get-Process -Name $param1 -IncludeUserName -ErrorAction SilentlyContinue| Where-Object UserName -Notlike $param3* | Select-Object -Expand UserName
 $countingFalse = $userProcsFalse | Measure-Object | Select -Expand count
@@ -27,36 +27,49 @@ $countingFalse = $userProcsFalse | Measure-Object | Select -Expand count
 
 if ($countProcs -eq 0){
     Write-Host "CRITICAL: Could not find any process named $param1"
-	exit 2}
+    exit 2
+    }
 elseif ($countingFalse -ge 0 ) {
-Write-Host "CRITICAL: Found $overallProcs $param1 process running. $countingFalse User which shouldn't run this program: $userProcsFalse"
-exit 2
-}
+    Write-Host "CRITICAL: Found $countProcs $param1 process running. $countingFalse User which shouldn't run this program: $userProcsFalse"
+    exit 2
+    }
 elseif ($countProcs -gt $param2){
- 	Write-Host "CRITICAL: Found more than $param2 process of $param1"
-	exit 2
-}
-else{
- Write-Host "OK: Found $param2 process running of $param1 .  All procs are run by user $param3"
- exit 0
+    Write-Host "CRITICAL: Found more than $param2 process of $param1"
+    exit 2
+    }
+elseif ($countProcs -lt $param2){
+    Write-Host "Warning: Found more than $param2 process of $param1"
+    exit 1
+    }
 
-}
-
-}
 
 else{
-if ($countProcs -le $param2){
-   if ($countProcs -eq 0){
-    Write-Host "CRITICAL: Could not find any process named $param1"
-	exit 2}
-   else{
+ if ($countProcs -eq $param2){
+   Write-Host "OK: Found $param2 process running of $param1 .  All procs are run by user $param3"
+   exit 0
+   }
 
-    Write-Host $countProcs
-	Write-Host "OK: Found $param2 process running of $param1"
-	exit 0}
 }
+
+# Queries without "desired user" input
+
+elseif{
+if ($countProcs -gt $param2){
+    Write-Host "CRITICAL: Found more than $param2 process of $param1"
+    exit 2
+    }
+elseif ($countProcs -lt $param2){
+    Write-Host "Warning: Found less than $param2 process of $param1"
+    exit 1
+    }
+else{
+    Write-Host "OK: Found $param2 process running of $param1"
+    exit 0
+    }
+}
+
 else{ 
-Write-Host "CRITICAL: Found more than $param2 process of $param1"
-exit 2
-}
+    Write-Host "Unknown: Something went wrong"
+    exit 3
+    }
 }
